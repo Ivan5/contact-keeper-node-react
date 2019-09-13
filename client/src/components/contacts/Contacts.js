44e41f9edde5154e8e1167a62c1fc9943e98ab17
Ivan5 <converse_ivan@hotmail.com>
2019-09-13 15:21:00 -0500

    Get clear contacts funct

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,25 +1,32 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import ContactItem from "./ContactItem";
 import ContactContext from "../../contacts/contact/contactContext";
 
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
 
-  const { contacts, filtered } = contactContext;
+  const { contacts, filtered, getContacts, loading } = contactContext;
 
-  if (contacts.lenght === 0) {
+  useEffect(() => {
+    getContacts();
+    // eslint-disable-next-line
+  }, []);
+
+  if (contacts !== null && contacts.lenght === 0 && !loading) {
     return <h4>Please add a contact</h4>;
   }
 
   return (
     <>
-      {filtered !== null
-        ? filtered.map(contact => (
-            <ContactItem key={contact._id} contact={contact} />
-          ))
-        : contacts.map(contact => (
-            <ContactItem key={contact._id} contact={contact} />
-          ))}
+      {contacts !== null && !loading
+        ? filtered !== null
+          ? filtered.map(contact => (
+              <ContactItem key={contact._id} contact={contact} />
+            ))
+          : contacts.map(contact => (
+              <ContactItem key={contact._id} contact={contact} />
+            ))
+        : null}
     </>
   );
 };
--- client/src/contacts/contact/ContatState.js
+++ client/src/contacts/contact/ContatState.js
@@ -3,10 +3,12 @@ import axios from "axios";
 import ContactContext from "./contactContext";
 import contactReducer from "./contactReducer";
 import {
+  GET_CONTACTS,
   ADD_CONTACT,
   DELETE_CONTACT,
   SET_CURRENT,
   CLEAR_CURRENT,
+  CLEAR_CONTACTS,
   UPDATE_CONTACT,
   FILTER_CONTACTS,
   CLEAR_FILTER,
@@ -15,13 +17,24 @@ import {
 
 const ContactState = props => {
   const initialState = {
-    contacts: [],
+    contacts: null,
     current: null,
     filtered: null,
     error: null
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  //Get contacts
+  const getContacts = async () => {
+    try {
+      const res = await axios.get("/api/contacts");
+
+      dispatch({ type: GET_CONTACTS, payload: res.data });
+    } catch (error) {
+      dispatch({ type: CONTACT_ERROR, payload: error.response.msg });
+    }
+  };
 
   //Add Contact
   const addContact = async contact => {
@@ -42,6 +55,11 @@ const ContactState = props => {
   //Delete Contact
   const deleteContact = id => {
     dispatch({ type: DELETE_CONTACT, payload: id });
+  };
+
+  // Clear Contacts
+  const clearContacts = () => {
+    dispatch({ type: CLEAR_CONTACTS });
   };
 
   //Set Current Contact
@@ -80,7 +98,9 @@ const ContactState = props => {
         clearCurrent,
         updateContact,
         filterContacts,
-        clearFilter
+        clearFilter,
+        getContacts,
+        clearContacts
       }}
     >
       {props.children}
